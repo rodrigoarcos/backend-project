07f618d16a29900e8dcafe24347f6aff78c92dce
rodsrose <rodsrose>
2021-06-27 11:36:23 +0200

    corrected script to load values

--- crm-db/restore-db.sh
+++ crm-db/restore-db.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 for collection in accounts contacts opportunities; do
-  docker exec -i crm_db_1 mongoimport /crm-data/$collection.json -u admin -p fullstack --type json -d crmsite -c $collection --headerline --drop --authenticationDatabase admin
+  docker exec -i crm-db_mongo_1 mongoimport /crm-data/$collection.json -u admin -p fullstack --type json -d crmsite -c $collection --jsonArray --drop --authenticationDatabase admin
 done
